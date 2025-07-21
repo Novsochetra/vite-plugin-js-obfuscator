a68f404e3c7a52736388edb40e3345f8192f4ea8
Novsochetra <sochetra12.nov@gmail.com>
2025-07-21 14:04:25 +0700

    chore: adding customObfuscatorPlugin and its preset

---
+++ README.md
@@ -0,0 +1,28 @@
+# vite-plugin-js-obfuscator
+
+A simple Vite plugin that obfuscates final JS bundles using [javascript-obfuscator](https://github.com/javascript-obfuscator/javascript-obfuscator)
+
+## Installation
+
+```bash
+npm install vite-plugin-custom-obfuscator --save-dev
+```
+
+## Usage
+
+```js
+// vite.config.js
+import viteJsObfuscator from "vite-plugin-js-obfuscator";
+
+export default {
+  plugins: [
+    viteJsObfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      debugProtection: true,
+      debugProtectionInterval: 1000,
+      selfDefending: true,
+    }),
+  ],
+};
+```
---
+++ index.js
@@ -0,0 +1,132 @@
+import JavaScriptObfuscator from "javascript-obfuscator";
+
+const defaultOptions = {
+  compact: true,
+  controlFlowFlattening: true,
+  splitStrings: true,
+  selfDefending: true,
+  debugProtection: true,
+  debugProtectionInterval: 1000,
+};
+
+export const highObfuscationLowPerformance = {
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 1,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 1,
+  debugProtection: true,
+  debugProtectionInterval: 4000,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: "hexadecimal",
+  log: false,
+  numbersToExpressions: true,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true,
+  splitStrings: true,
+  splitStringsChunkLength: 5,
+  stringArray: true,
+  stringArrayCallsTransform: true,
+  stringArrayEncoding: ["rc4"],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 5,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 5,
+  stringArrayWrappersType: "function",
+  stringArrayThreshold: 1,
+  transformObjectKeys: true,
+  unicodeEscapeSequence: false,
+};
+
+export const mediumObfuscationOptimalPerformance = {
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 0.75,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 0.4,
+  debugProtection: false,
+  debugProtectionInterval: 0,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: "hexadecimal",
+  log: false,
+  numbersToExpressions: true,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true,
+  splitStrings: true,
+  splitStringsChunkLength: 10,
+  stringArray: true,
+  stringArrayCallsTransform: true,
+  stringArrayCallsTransformThreshold: 0.75,
+  stringArrayEncoding: ["base64"],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 2,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 4,
+  stringArrayWrappersType: "function",
+  stringArrayThreshold: 0.75,
+  transformObjectKeys: true,
+  unicodeEscapeSequence: false,
+};
+
+export const lowObfuscationHighPerformanceConfig = {
+  compact: true,
+  controlFlowFlattening: false,
+  deadCodeInjection: false,
+  debugProtection: false,
+  debugProtectionInterval: 0,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: "hexadecimal",
+  log: false,
+  numbersToExpressions: false,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true,
+  splitStrings: false,
+  stringArray: true,
+  stringArrayCallsTransform: false,
+  stringArrayEncoding: [],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 1,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 2,
+  stringArrayWrappersType: "variable",
+  stringArrayThreshold: 0.75,
+  unicodeEscapeSequence: false,
+};
+
+export function customObfuscatorPlugin(options = defaultOptions) {
+  return {
+    name: "vite-js-obfuscator",
+    apply: "build",
+
+    generateBundle(_, bundle) {
+      console.log("\n\n📦 Starting obfuscation of JS chunks...\n");
+
+      for (const [fileName, chunk] of Object.entries(bundle)) {
+        console.log(`obfuscating: ${fileName}`);
+        if (chunk.type === "chunk" && fileName.endsWith(".js")) {
+          try {
+            const obfuscationResult = JavaScriptObfuscator.obfuscate(
+              chunk.code,
+              options
+            );
+
+            chunk.code = obfuscationResult.getObfuscatedCode();
+          } catch (err) {
+            this.warn(`Obfuscation failed for ${fileName}: ${err.message}`);
+          }
+        }
+      }
+
+      console.log(`\n✅ Obfuscation complete.\n`);
+    },
+  };
+}
